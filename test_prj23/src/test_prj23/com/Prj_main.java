2e448e9d5337dc412cb59217af0b241942ee3f9e
jungwon-prj <ghddhfn1858@naver.com>
2024-08-29 17:56:25 +0900

    두 번째 커밋 테스트

--- test_prj23/src/test_prj23/com/Prj_main.java
+++ test_prj23/src/test_prj23/com/Prj_main.java
@@ -4,6 +4,9 @@ public class Prj_main {
 
 	public static void main(String[] args) {
 		System.out.println("이것은 깃허브테스트여");
+		System.out.println("이것은 깃허브테스트여");
+		System.out.println("이것은 깃허브테스트여");
+		System.out.println("이것은 깃허브테스트여");
 
 	}
 
